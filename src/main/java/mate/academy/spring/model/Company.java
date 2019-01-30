faff79193bfe8b49012ca2c0fee6a7a91f6ef0d3
Anatolii <dinexpod@gmail.com>
2019-01-30 15:50:17 +0200

    -added all to branch

--- src/main/java/mate/academy/spring/model/Company.java
+++ src/main/java/mate/academy/spring/model/Company.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Set;
 
 @Entity
 @Data
